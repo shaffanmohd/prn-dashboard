af2df614f0ca03e8c97a100ba12483849b772823
Shaffan-Mohd <shaffan.mohd@maybank.com>
2026-08-20 16:26:35 +0800

    add  vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -20,6 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
+      <Analytics />
       <body className="antialiased">{children}</body>
     </html>
   );
